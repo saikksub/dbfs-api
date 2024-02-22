a7d4b76d291567fdfe848e948edc2290e2cc1d51
Sai K K <saikksub@gmail.com>
2024-02-22 20:14:56 +0530

    Adds get-list method

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export {getStatus} from './lib/get-status.js';
 
+export {getList} from './lib/get-list.js';
+
 export const connect = async function ({url, token}) {
 	try {
 		axios.defaults.baseURL = url;
--- test.js
+++ test.js
@@ -1,10 +1,21 @@
+import process from 'node:process';
 import test from 'ava';
-import { connect, getStatus } from './index.js';
+import dotenv from 'dotenv';
+import {connect, getStatus, getList} from './index.js';
 
-test('main', async t => {
+dotenv.config();
+
+test('Connect and get status', async t => {
 	t.is(await connect({
 		url: process.env.BASE_URL,
-		token: process.env.TOKEN
+		token: process.env.TOKEN,
 	}), true);
-	t.is(await getStatus(), true);
+
+	const rootDirStatus = await getStatus();
+	t.is(rootDirStatus.is_dir, true);
+});
+
+test('Llist files from root', async t => {
+	const listRootDir = await getList('/');
+	t.is(Array.isArray(listRootDir.files), true);
 });
