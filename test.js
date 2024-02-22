ef90740123a0c00a7a199110b47fe2943abeef6e
Sai K K <saikksub@gmail.com>
2024-02-22 19:44:23 +0530

    remove creds

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 import test from 'ava';
-import {connect, getStatus} from './index.js';
+import { connect, getStatus } from './index.js';
 
 test('main', async t => {
-	t.is(await connect({url: 'https://5809494911638817.7.gcp.databricks.com/api/2.0/dbfs',
-		token: 'dapie5e1e2e053b5fbadf5a6e23a0d8b0963'}), true);
+	t.is(await connect({
+		url: process.env.BASE_URL,
+		token: process.env.TOKEN
+	}), true);
 	t.is(await getStatus(), true);
 });
